2c6d27fac256c9ab95c58e7fe5fb8bbeaacf9eff
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 06:13:01 +0900

    Update Db

---
+++ Db/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module Db {
+    requires jdk.jsobject;
+    requires java.sql;
+    requires javafx.web;
+    
+    requires com.google.gson;
+}
