5ec75584f253e066147c89b877cd0449bf9bbbb5
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 06:06:52 +0900

    Update Jasper

---
+++ Jasper/src/main/java/module-info.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module Jasper {
+    requires jdk.jsobject;
+    requires java.desktop;
+    requires javafx.web;
+    
+    requires jasperreports;
+    requires commons.beanutils;
+    requires commons.collections;
+    requires commons.digester;
+    requires commons.logging;
+    requires org.apache.commons.collections4;
+    requires itext;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.util;
+    requires jcommon;
+    requires jfreechart;
+    requires ecj;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.dataformat.xml;
+    requires org.codehaus.stax2;
+    requires com.ctc.wstx;
+    requires com.google.gson;
+}
