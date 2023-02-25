9e4ad9827ddb88a501a222a3846e09f86cf4280a
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 06:08:35 +0900

    Update Ftp

---
+++ Ftp/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module Ftp {
+    requires jdk.jsobject;
+    requires javafx.web;
+    
+    requires org.apache.commons.net;
+    requires com.google.gson;
+}
