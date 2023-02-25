b56ab9a3edf354c70ea8f1796d5c3b45d373dad8
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 06:15:02 +0900

    Update CapAiServer

---
+++ CapAiServer/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module CapAiServer {
+    requires jdk.jsobject;
+    requires java.desktop;
+    requires javafx.web;
+    
+    requires com.google.gson;
+    requires netty.all;
+}
