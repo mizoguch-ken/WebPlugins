5ff0741be36f042bfdd0c65fc6c027b0d09c77f8
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 05:58:55 +0900

    Update Serial

---
+++ Serial/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module Serial {
+    requires jdk.jsobject;
+    requires javafx.web;
+    
+    requires com.google.gson;
+    requires nrjavaserial;
+    requires commons.net;
+}
