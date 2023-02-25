82f007109bf139680fa6be68a94fdbe477800030
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 06:10:08 +0900

    Update File

---
+++ File/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module File {
+    requires jdk.jsobject;
+    requires javafx.web;
+    
+    requires com.google.gson;
+}
