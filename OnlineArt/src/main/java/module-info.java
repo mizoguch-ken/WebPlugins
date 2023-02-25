960f7662ae9da75b4d765acd35c16dde782a1a71
mizoguch-ken <31312266+mizoguch-ken@users.noreply.github.com>
2023-02-26 06:02:14 +0900

    Update OnlineArt

---
+++ OnlineArt/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module OnlineArt {
+    requires jdk.jsobject;
+    requires javafx.web;
+    requires javafx.swing;
+    
+    requires com.google.gson;
+    requires nrjavaserial;
+    requires commons.net;
+}
